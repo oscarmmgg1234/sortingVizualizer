a2469f931b4df98446fec6959daf2bf7c1584bff
Oscar Maldonado <oscarmmgg1234@gmail.com>
2020-12-25 14:19:18 -0800

    componentsMagik

---
+++ src/components/sideBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class SideBar extends React.Component{
+    render(){
+        return(
+        <div>Oscar</div>
+        )  }
+}
---
+++ src/views/homeVisualizer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import SideBar from "../components/sideBar";
+
+const Home = () => {
+
+
+    return (
+<div className={"HomeContainer"} style={{styles}}>
+<SideBar/>
+</div>
+    )
+}
+
+const styles = {
+    flexDirection: "row"
+
+}
+
+
+export default Home;
